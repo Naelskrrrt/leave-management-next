4cc49d8c55ccbb6ec704ac5c3a74cf3fd611d12f
NaelSkrrrt <ralalanael@gmail.com>
2024-04-03 00:17:17 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,25 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return <div className="grid w-full p-4 bg-black grid-cols-12 grid-rows-6 relative gap-4" 
+              style={{
+              height: "calc(100vh - 100px)"
+        }}>
+    <div className="col-span-3 bg-red-500 row-span-2 p-3 rounded-lg">
+        qlq chose
+    </div>
+    <div className="col-span-3 bg-red-500 row-span-2 p-3 rounded-lg">
+        qlq chose
+    </div>
+    <div className="col-span-6 bg-red-500 row-span-4 p-3 rounded-lg">
+        qlq chose
+    </div>
+    <div className="col-span-6 bg-red-500 row-span-4 p-3 rounded-lg">
+        qlq chose
+    </div>
+    <div className="col-span-6 bg-red-500 row-span-2 p-3 rounded-lg">
+        qlq chose
+    </div>
+
+  </div>;
 }
